991c7e2623d653eb12093d045b7b1bea48e70135
AmandaBarros94 <mandy.bjj1@gmail.com>
2022-01-26 15:17:15 -0300

    feito um for each dando push pra um array do elemento com id correto

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const species = [];
+  ids.forEach((id) => {
+    species.push(data.species.find((specie) => specie.id === id));
+  });
+  return species;
 }
-
 module.exports = getSpeciesByIds;
