7d71be794bd1cce5982381a4dda6af1acd27983f
AmandaBarros94 <mandy.bjj1@gmail.com>
2022-01-31 00:34:37 -0300

    fiz uma procura pela especie do animal e verifiquei quantos haviam retornando o valor

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,18 @@
-const data = require('../data/zoo_data');
+const {species} = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal && animal.specie && animal.sex) {
+    return species
+      .find((kind) => kind.name === animal.specie).residents
+      .filter((kind) => kind.sex === animal.sex).length;
+  }
+  if (animal) {
+    return species.find((kind) => kind.name === animal.specie).residents.length;
+  }
+  return species.reduce((acc, kind) => {
+    acc[kind.name] = kind.residents.length;
+    return acc;
+  }, {});
 }
 
 module.exports = countAnimals;
