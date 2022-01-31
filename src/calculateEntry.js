f735d7f367556c680611e21809ba1d8101f60fd6
AmandaBarros94 <mandy.bjj1@gmail.com>
2022-01-31 00:53:16 -0300

    gerado o objeto com a quantidade de pessoas visitantes e cauculado o valor das entradas

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,33 @@
-const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const amount = {
+    child: 0,
+    adult: 0,
+    senior: 0,
+  };
+  entrants.forEach((visitants) => {
+    if (visitants.age < 18) {
+      amount.child += 1;
+    } else if (visitants.age >= 18 && visitants.age < 50) {
+      amount.adult += 1;
+    } else {
+      amount.senior += 1;
+    }
+  });
+  return amount;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || !entrants.length) {
+    return 0;
+  }
+  const cost = {
+    child: prices.child * countEntrants(entrants).child,
+    adult: prices.adult * countEntrants(entrants).adult,
+    senior: prices.senior * countEntrants(entrants).senior,
+  };
+  const total = Object.values(cost).reduce((acc, value) => acc + value, 0);
+  return total;
 }
-
 module.exports = { calculateEntry, countEntrants };
