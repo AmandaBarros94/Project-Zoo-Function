84066200fb9b924c66d89edc1aa7b3dccf21918f
AmandaBarros94 <mandy.bjj1@gmail.com>
2022-02-01 00:12:13 -0300

    executada a função que procura o animal mais velho e o funcionario que cuida dele, com o find e reduce

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,17 @@
-const data = require('../data/zoo_data');
+const { employees, species } = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+function getOldest(id) {
+  const getSenior = (acc, resident) => {
+    if (acc.age > resident.age) {
+      return acc;
+    }
+    return resident;
+  };
+  const { residents } = species.find((specie) => specie.id === id);
+  return Object.values(residents.reduce(getSenior));
 }
 
+function getOldestFromFirstSpecies(id) {
+  return getOldest(employees.find((employee) => employee.id === id).responsibleFor[0]);
+}
 module.exports = getOldestFromFirstSpecies;
