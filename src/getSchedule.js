30c0bef3424c240960f3e5c9fb3f765458256228
AmandaBarros94 <mandy.bjj1@gmail.com>
2022-02-01 00:41:00 -0300

    feita a função que localiza dia e horário de exibição de acordo com a agenda ultilizando o find, foreach e map

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,46 @@
-const data = require('../data/zoo_data');
+const { hours, species } = require('../data/zoo_data');
+
+const openDays = Object.keys(hours);
+const operation = Object.values(hours);
+const animalsName = species.map((specie) => specie.name);
+const object1 = {};
+
+function allAnimals(weekDay) {
+  const exhibit = species.filter(({ availability }) => availability.includes(weekDay));
+  return exhibit.map((item) => item.name);
+}
+
+function diary() {
+  openDays.forEach((element, index) => {
+    object1[openDays[index]] = {
+      officeHour: `Open from ${operation[index].open}am until ${operation[index].close}pm`,
+      exhibition: allAnimals(openDays[index]),
+    };
+  });
+
+  operation.forEach((time, index) => {
+    if (time.open === 0) {
+      object1[openDays[index]] = {
+        officeHour: 'CLOSED',
+        exhibition: 'The zoo will be closed!',
+      };
+    }
+  });
+
+  return object1;
+}
 
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  if (scheduleTarget === undefined) {
+    return diary();
+  }
+  if (openDays.includes(scheduleTarget)) {
+    return { [scheduleTarget]: object1[scheduleTarget] };
+  }
+  if (animalsName.includes(scheduleTarget)) {
+    return species.find((specie) => specie.name === scheduleTarget).availability;
+  }
+  return diary();
 }
 
 module.exports = getSchedule;
